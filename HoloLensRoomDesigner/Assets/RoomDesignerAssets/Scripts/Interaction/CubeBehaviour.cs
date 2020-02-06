a0d952e32ffd96c92f1310c6b462e1171eb62a1c
_ChrisP_ <HdJdh62xZurb5gtXM5IB>
2020-02-06 13:42:27 +0100

    - missing data for inital commit - added /Builds/ to .gitignore

---
+++ HoloLensRoomDesigner/Assets/RoomDesignerAssets/Scripts/Interaction/CubeBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine;
+
+namespace RoomDesignerAssets.Scripts.Interaction
+{
+    public class CubeBehaviour : MonoBehaviour
+    {
+
+        private MeshRenderer _meshRenderer;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            _meshRenderer = GetComponent<MeshRenderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public void SayHello()
+        {
+            Debug.Log("Hello!");
+        }
+    }
+}
