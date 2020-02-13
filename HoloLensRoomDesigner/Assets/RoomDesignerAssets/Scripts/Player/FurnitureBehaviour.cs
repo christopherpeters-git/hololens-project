aa49a9cffaf999cfe392c56b77aaab4fd464fa7c
Christopher Peters <chrissi.peters98@web.de>
2020-02-13 13:48:06 +0100

    -Updated MRTK - Change SpatialMappingTest scene

--- HoloLensRoomDesigner/Assets/RoomDesignerAssets/Scripts/Player/FurnitureBehaviour.cs
+++ HoloLensRoomDesigner/Assets/RoomDesignerAssets/Scripts/Player/FurnitureBehaviour.cs
@@ -4,10 +4,13 @@ using System.Collections.Generic;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.UI;
 using UnityEngine;
+using UnityEngine.XR.WSA;
 
 public class FurnitureBehaviour : MonoBehaviour
 {
     public FurnitureType type;
+
+    public float placeRange;
     
     public bool moved { get; set; }
 
@@ -25,7 +28,23 @@ public class FurnitureBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DetectCollisions();
     }
+
+    void DetectCollisions()
+    {
+        int layerMask = 1 << LayerMask.NameToLayer("SpatialSurface");
+        Vector3[] directions = {Vector3.up, Vector3.down, Vector3.forward, Vector3.back, Vector3.left, Vector3.right};
+        foreach (var dir in directions)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, dir, out hit, placeRange))
+            {
+                Debug.Log("Mesh Hit!");
+            }
+            Debug.DrawRay(transform.position,dir,Color.green, placeRange);
+        }
+    }
+
 
 }
