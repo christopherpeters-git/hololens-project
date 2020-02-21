1ef9fa140f3926734d8f7926c76ac706c1361ccc
_ChrisP_ <HdJdh62xZurb5gtXM5IB>
2020-02-21 16:47:46 +0100

    -WIP photo mode -Added button to hide UI -Added button to go back to UI

---
+++ HoloLensRoomDesigner/Assets/RoomDesignerAssets/Scripts/UI/ButtonBackToUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBackToUI : MonoBehaviour
+{
+    [SerializeField] private ButtonPhotoMode _photoMode;
+
+    public void ToggleUI()
+    {
+        _photoMode.ToggleUI();
+    }
+}
---
+++ HoloLensRoomDesigner/Assets/RoomDesignerAssets/Scripts/UI/ButtonPhotoMode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPhotoMode : MonoBehaviour
+{
+    [SerializeField] private GameObject _ui;
+    [SerializeField] private GameObject _buttonBackToUI;
+    private bool _isInPhotoMode;
+
+    /// <summary>
+    /// Called by Button; Toggles the UI for screenshots
+    /// </summary>
+    public void ToggleUI()
+    {
+        if (_ui.activeSelf)
+        {
+            _ui.SetActive(false);
+            _buttonBackToUI.SetActive(true);
+        }
+        else
+        {
+            _buttonBackToUI.SetActive(false);
+            _ui.SetActive(true);
+        }
+        Debug.Log(_ui.activeSelf);
+    }
+}
